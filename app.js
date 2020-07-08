fe79d074341465b7ba07e3a767e884d8e6e3e0e6
James Hayes <owner@jamesdewitthayes.com>
2020-07-07 22:24:03 -0700

    adding graphql schemas

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.user(bodyParser.json())
+app.use(bodyParser.json())
+
+app.user('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `),
+    rootValue: {
+        events: () => {
+            return ['Romantic', 'Sailing', 'Coding']
+        },
+        createEvent: ()
+    }
+}));
 
 app.listen(3000);
