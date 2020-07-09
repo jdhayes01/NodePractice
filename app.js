0081b686e4f9cdd1fa85049952b51f709b0ac01c
James Hayes <owner@jamesdewitthayes.com>
2020-07-08 18:40:12 -0700

    fiexed express-graphql import error

--- app.js
+++ app.js
@@ -5,29 +5,36 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.user('/graphql', graphqlHttp({
-    schema: buildSchema(`
-        type RootQuery {
-            events: [String!]!
-        }
+app.use(
+    '/graphql',
+    graphqlHttp({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
 
-        type RootMutation {
-            createEvent(name: String): String
-        }
+            type RootMutation {
+                createEvent(name: String): String
+            }
 
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
         `),
-    rootValue: {
-        events: () => {
-            return ['Romantic', 'Sailing', 'Coding']
+        rootValue: {
+            events: () => {
+                return ['Romantic', 'Sailing', 'Coding'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            }
         },
-        createEvent: ()
-    }
-}));
+        graphiql: true
+    })
+);
 
 app.listen(3000);
